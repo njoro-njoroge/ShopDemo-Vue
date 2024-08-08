fd35d25cc3f50398d6c212b314eeaf26d10e6e94
njoro-njoroge <njoro.njoroge02@gmail.com>
2024-08-08 18:26:53 +0300

    added vuex useStore

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import './assets/main.css'
 
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';  // Make sure this path is correct
+import router from './router';
+import store from './store'; // Import your Vuex store
 
 const app = createApp(App);
+app.use(store);
 app.use(router);
 app.mount('#app');
 
