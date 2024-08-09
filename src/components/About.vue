24159efd62a22876f640ad9f3e5a8b8f9d2c91a3
njoro-njoroge <njoro.njoroge02@gmail.com>
2024-08-09 19:46:14 +0300

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="about-us-container">
+      <div class="company-info">
+        <h1>About Us</h1>
+        <img src="https://via.placeholder.com/800x400?text=DemoShop" alt="DemoShop" class="about-image"/>
+       
+        <p class="intro">
+          Welcome to Demo<span class="brand-name">S</span>hop, your premier destination for
+          electronics, fashion, and more. We're dedicated to delivering the finest products with a focus on
+          dependability, customer service, and uniqueness.
+        </p> <p>
+          Founded in <strong>2020</strong>, ShopSmart started from a small home office with a
+          mission to provide top-notch products. Our journey began with extensive research and sourcing
+          to find the best items, and today, we proudly serve customers across the globe through our
+          online platform.
+        </p>
+        <p>
+          At ShopSmart, we believe in offering only the highest quality products combined with
+          exceptional customer service. Our dedicated team is here to assist you with any inquiries or
+          concerns. We're passionate about our work and grateful for the opportunity to share it with
+          you.
+        </p>
+        <p>
+          We constantly update our product range to ensure that we bring you the latest and greatest
+          in electronics, fashion, and more. Our commitment to quality means that you can trust
+          ShopSmart to meet and exceed your expectations.
+        </p>
+       <p>
+          Thank you for being a part of our journey. We look forward to continuing to serve you with
+          the best products and outstanding service.
+        </p>
+      </div>
+      <div class="contact-us">
+        <h2>Contact Us</h2>
+        <p>If you have any questions or need assistance, feel free to reach out:</p>
+        <ul>
+          <li><strong>Email:</strong> support@shopsmart.com</li>
+          <li><strong>Phone:</strong> +254 (072) 123-4567</li>
+          <li><strong>Address:</strong> 1234 Commerce St, Suite 567, Business City, BC 89012</li>
+        </ul>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    setup() {
+      // Component logic here if needed
+    },
+  }
+  </script>
+  
+  <style scoped>
+ 
+  </style>
+  
---
+++ src/components/CartButton.vue
@@ -0,0 +1,75 @@
+<template>
+    <router-link
+     class="floating-cart-button"
+     to="/cart">
+      <img src="@/assets/cart.png" alt="Cart" class="cart-icon" />
+      <span class="cart-count">{{ cartCount }}</span>
+    </router-link>
+  </template>
+  
+  <script>
+  import { computed } from 'vue';
+  import { useStore } from 'vuex';
+  
+  export default {
+    name: 'FloatingCartButton',
+    setup() {
+      const store = useStore();
+      const cartCount = computed(() => store.getters.cartCount);
+  
+      console.log("Cart items:", cartCount.value);
+  
+      return {
+        cartCount,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .floating-cart-button {
+    position: fixed;
+    bottom: 2rem;
+    right: 1rem;
+    width: 60px;
+    height: 60px;
+    border: 1px solid #040101;
+    background-color: whitesmoke; 
+    color: #fff;
+    border-radius: 50%;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    transition: background-color 0.3s, transform 0.3s;
+  }
+  
+  .floating-cart-button:hover {
+    transform: translateY(-5px);
+    box-shadow: 0 8px 16px rgba(244, 3, 3, 0.5);
+  }
+  
+  .cart-icon {
+    width: 30px;
+    height: 30px;
+    z-index: -1;
+  }
+  
+  .cart-count {
+    background-color: #fff;
+    color: #ff5722;
+    border-radius: 50%;
+    border: 1px solid #040101;
+    width: 24px;
+    height: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 0.9rem;
+    font-weight: bold;
+    margin-top: -2rem;
+    margin-left: -0.1rem;
+  }
+  </style>
+  
